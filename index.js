4c2c9d28fe1f5c5da5230ffebabf670cdaf31af5
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-09-27 17:21:08 +0530

    change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 const app = express();
 
 // config dotenv
